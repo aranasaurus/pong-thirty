00eead5341b842e0de15569617cf720d97e90578
Ryan Arana <ryan@ryanarana.com>
2015-09-01 01:50:04 -0700

    It's Pong:30

---
+++ pong-thirty/GameScene.swift
@@ -0,0 +1,34 @@
+//
+//  GameScene.swift
+//  pong:30
+//
+//  Created by Ryan Arana on 9/1/15.
+//  Copyright (c) 2015 OK, Robot Studios. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    let paddle1 = Paddle(position: CGPoint(x: 50, y: 0))
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        paddle1.position = CGPoint(x: 50, y: (view.bounds.height - Paddle.defaultPaddleSize.height)/2.0)
+        addChild(paddle1)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        /* Called when a touch begins */
+        if let touch = touches.first where touch.locationInView(self.view!).x < self.view!.bounds.width/2.0 {
+            paddle1.touchesBegan(touches, withEvent: event)
+        }
+    }
+
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        paddle1.touchesMoved(touches, withEvent: event)
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+        paddle1.update()
+    }
+}
--- pong-thirty/GameViewController.swift
+++ pong-thirty/GameViewController.swift
@@ -14,9 +14,10 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let scene = GameScene(fileNamed:"GameScene") {
+        if let skView = self.view as? SKView {
+            let scene = GameScene(size: skView.bounds.size)
+
             // Configure the view.
-            let skView = self.view as! SKView
             skView.showsFPS = true
             skView.showsNodeCount = true
             
@@ -35,16 +36,13 @@ class GameViewController: UIViewController {
     }
 
     override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return .AllButUpsideDown
-        } else {
-            return .All
-        }
+        return .Landscape
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
+        print("==== didReceiveMemoryWarning! ====")
     }
 
     override func prefersStatusBarHidden() -> Bool {
